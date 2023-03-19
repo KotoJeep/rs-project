d678583c9c74313e7d3687a6841d47186a44f651
KotoJeep <omaigader@gmail.co>
2023-03-19 13:19:06 +0700

    feat: add card & font

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { MouseEventHandler, ReactNode } from 'react';
+import './Card.scss';
 
 export type CardProps = {
   image: string;
-  title: React.ReactNode;
-  subtitle: React.ReactNode;
-  content?: React.ReactNode;
-  onClick?: React.MouseEventHandler;
+  category?: ReactNode;
+  title: ReactNode;
+  description: ReactNode;
+  price: ReactNode;
+  onClick?: MouseEventHandler;
 };
 
-const Card = (): CardProps => {
-  return <div></div>;
+const Card = ({ image, title, category, description, price, onClick }: CardProps) => {
+  return (
+    <div className="card" onClick={onClick}>
+      <img src={image} className="card__img" />
+      <div className="card__wrapper">
+        <h5 className="card__category">{category}</h5>
+        <h3 className="card__title">{title}</h3>
+        <p className="card__description">{description}</p>
+        <h3 className="card__price">${price}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,9 +3,17 @@ import Card from '../../components/Card';
 
 const MainPage = () => {
   return (
-    <>
-      <Card />
-    </>
+    <div style={{ padding: '50px' }}>
+      <Card
+        image={'https://api.lorem.space/image/fashion?w=640&h=480&r=7346'}
+        title={'Unbranded Plastic Ball'}
+        description={
+          'The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design'
+        }
+        category={'Clothes'}
+        price={379}
+      />
+    </div>
   );
 };
 
