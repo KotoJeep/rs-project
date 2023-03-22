6081de48d6e1d6b4c0a4a2696178995de7769763
ВОЛКОВ АНТОН АЛЕКСАНДРОВИЧ <omaigader@gmail.com>
2023-03-22 09:46:14 +0700

    fix: eslint warning

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { FC, MouseEventHandler, ReactNode } from 'react';
+import React, { MouseEventHandler, ReactNode } from 'react';
 import './Card.scss';
 
 export type CardProps = {
--- src/components/CardsWrapper/CardsWrapper.tsx
+++ src/components/CardsWrapper/CardsWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import './CardsWrapper.scss';
 import Card, { CardProps } from '../Card';
 
